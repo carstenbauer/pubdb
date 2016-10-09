97e6f843bdf046d71b7f736012ecacf229d5e227
crstnbr <mail@carstenbauer.eu>
2016-10-09 13:25:24 +0200

    Mouseover logic for publication options (Update, BibTex etc.))

--- show.php
+++ show.php
@@ -12,34 +12,7 @@
 	$publications = $db->getPublications();
 	?>
 
-	<script type="text/javascript" src="js/script.js"></script>
 
-	<script type="text/javascript">
-		function byProject(pub_obj){
-			if (pub_obj.projects.toString().includes(this.toString()))
-				return true;
-			else
-				return false;
-		}
-
-		function printPublication(pub) {
-	    	pubp.innerHTML = pubp.innerHTML + PublicationToHTMLString(pub) + "<br><br>"; 
-		}
-
-		function DoFilter(project) {
-			var pubs = <?php echo json_encode($publications); ?>;
-			if (project != "all")
-				pubs = pubs.filter(byProject, project);
-
-			if (Object.keys(pubs).length > 0) {
-				pubp.innerHTML = "";
-				pubs.forEach(printPublication);
-				MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-			} else {
-				pubp.innerHTML = "0 results.";
-			}
-		}
-	</script>
 
 </head>
 <body onload='DoFilter("all");'>
@@ -74,6 +47,52 @@
 		</div>
 
 	</div>
+
+
+<!-- Javascript !-->
+<script type="text/javascript" src="js/script.js"></script>
+
+	<script type="text/javascript">
+
+		var pubs = <?php echo json_encode($publications); ?>;
+
+		function showOptions(id){
+			var pub = pubs.filter(function(x) { return x.id.toString()==id; })[0];
+			var puboptions = document.getElementById("pub".concat(pub.id.toString()).concat("options"));
+			puboptions.removeAttribute("hidden");
+		}
+
+		function hideOptions(id){
+			var pub = pubs.filter(function(x) { return x.id.toString()==id; })[0];
+			var puboptions = document.getElementById("pub".concat(pub.id.toString()).concat("options"));
+			puboptions.setAttribute("hidden", true);
+		}
+
+		function byProject(pub_obj){
+			if (pub_obj.projects.toString().includes(this.toString()))
+				return true;
+			else
+				return false;
+		}
+
+		function printPublication(pub) {
+	    	pubp.innerHTML = pubp.innerHTML + "<span id='pub" + pub.id.toString() + "' class=publication onmouseover='showOptions(".concat(pub.id.toString()).concat(")' onmouseout='hideOptions(").concat(pub.id.toString()).concat(")'>") + PublicationToHTMLString(pub) + "<span id='pub" + pub.id.toString() + "options' class=publication-options hidden>[<a href=?sec=insert&	mode=update>Update</a>, <a>BibTex</a>]</span></span>" + "<br><br>"; 
+		}
+
+		function DoFilter(project) {
+			var pubsf = pubs;
+			if (project != "all")
+				pubsf = pubs.filter(byProject, project);
+
+			if (Object.keys(pubsf).length > 0) {
+				pubp.innerHTML = "";
+				pubsf.forEach(printPublication);
+				MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+			} else {
+				pubp.innerHTML = "0 results.";
+			}
+		}
+	</script>
 
 </body>
 
