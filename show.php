2ff472ad42c1ca2c0c07cd69af1e2a09efdf797e
crstnbr <mail@carstenbauer.eu>
2016-10-16 14:31:59 +0200

    Redesign of filter box; Now small bar over publications

--- show.php
+++ show.php
@@ -18,33 +18,25 @@
 <body onload='DoFilter("all");'>
 
 	<div id="container">	
-
-		<div id="main">
 			<br>
 			<h2>Publications</h2>
-			<br>
+
+			<span id="filter">
+				<a id=showall class=projectfilter href='javascript:DoFilter("all")' class="small">Show All</a> or 
+				
+					Filter by project: 
+					<?php
+					$projects = $db->getProjects();
+					foreach($projects as $project){
+					    echo "<a id=".$project["abbrev"]." class=projectfilter href='javascript:DoFilter(\"".$project["abbrev"]."\");'>".$project["abbrev"]. "</a> ";
+					}
+
+
+					?>
+			</span>
 
 			<p id="pubp">
 			</p>
-		</div>
-
-		<div id="filter">
-			<br>
-			<a href='javascript:DoFilter("all")' class="small">Show All</a>
-			<br>
-
-			<p class="small">
-				<?php
-
-				$projects = $db->getProjects();
-				foreach($projects as $project){
-				    echo "<a href='javascript:DoFilter(\"".$project["abbrev"]."\");'>".$project["abbrev"]. "</a>: " . $project["title"]. " <br>";
-				}
-
-
-				?>
-			</p>
-		</div>
 
 	</div>
 
@@ -59,13 +51,13 @@
 		function showOptions(id){
 			var pub = pubs.filter(function(x) { return x.id.toString()==id; })[0];
 			var puboptions = document.getElementById("pub".concat(pub.id.toString()).concat("options"));
-			puboptions.removeAttribute("hidden");
+			puboptions.style.visibility = "visible";
 		}
 
 		function hideOptions(id){
 			var pub = pubs.filter(function(x) { return x.id.toString()==id; })[0];
 			var puboptions = document.getElementById("pub".concat(pub.id.toString()).concat("options"));
-			puboptions.setAttribute("hidden", true);
+			puboptions.style.visibility = "hidden";
 		}
 
 		function openBibTexModal(id){
@@ -82,15 +74,27 @@
 
 		function printPublication(pub) {
 			if (pub.bibtex.toString()!="")
-				pubp.innerHTML = pubp.innerHTML + "<span id='pub" + pub.id.toString() + "' class=publication onmouseover='showOptions(".concat(pub.id.toString()).concat(")' onmouseout='hideOptions(").concat(pub.id.toString()).concat(")'>") + PublicationToHTMLString(pub) + "<span id='pub" + pub.id.toString() + "options' class=publication-options hidden>[<a href=?sec=update&id=".concat(pub.id.toString()) + ">Update</a>, <a href='javascript:openBibTexModal(".concat(pub.id.toString()) + ")'>BibTeX</a>]</span></span>" + "<br><br>";
+				pubp.innerHTML = pubp.innerHTML + "<span id='pub" + pub.id.toString() + "' class=publication onmouseover='showOptions(".concat(pub.id.toString()).concat(")' onmouseout='hideOptions(").concat(pub.id.toString()).concat(")'>") + PublicationToHTMLString(pub) + "<span id='pub" + pub.id.toString() + "options' class=publication-options style=\"visibility: hidden;\">[<a href=?sec=update&id=".concat(pub.id.toString()) + ">Update</a>, <a href='javascript:openBibTexModal(".concat(pub.id.toString()) + ")'>BibTeX</a>]</span></span>" + "<br>";
 			else 
-				pubp.innerHTML = pubp.innerHTML + "<span id='pub" + pub.id.toString() + "' class=publication onmouseover='showOptions(".concat(pub.id.toString()).concat(")' onmouseout='hideOptions(").concat(pub.id.toString()).concat(")'>") + PublicationToHTMLString(pub) + "<span id='pub" + pub.id.toString() + "options' class=publication-options hidden>[<a href=?sec=update&id=".concat(pub.id.toString()) + ">Update</a>]</span></span>" + "<br><br>";
+				pubp.innerHTML = pubp.innerHTML + "<span id='pub" + pub.id.toString() + "' class=publication onmouseover='showOptions(".concat(pub.id.toString()).concat(")' onmouseout='hideOptions(").concat(pub.id.toString()).concat(")'>") + PublicationToHTMLString(pub) + "<span id='pub" + pub.id.toString() + "options' class=publication-options style=\"visibility: hidden;\">[<a href=?sec=update&id=".concat(pub.id.toString()) + ">Update</a>]</span></span>" + "<br>";
 		}
 
 		function DoFilter(project) {
 			var pubsf = pubs;
-			if (project != "all")
+			if (project != "all"){
 				pubsf = pubs.filter(byProject, project);
+				var elements = document.getElementsByClassName("projectfilter");
+			    for (var i = 0; i < elements.length; i++) {
+ 			   		elements[i].style.fontWeight = "300";
+   				}
+				document.getElementById(project).style.fontWeight = "bold";
+			} else {
+				var elements = document.getElementsByClassName("projectfilter");
+			    for (var i = 0; i < elements.length; i++) {
+ 			   		elements[i].style.fontWeight = "300";
+   				}
+   				document.getElementById("showall").style.fontWeight = "bold";
+			}
 
 			if (Object.keys(pubsf).length > 0) {
 				pubp.innerHTML = "";
